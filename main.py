2a395079574f25566a8dfe8e03a0f4050da5512f
uqarni <108438834+uqarni@users.noreply.github.com>
2024-02-12 18:48:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pytz
 #connect to supabase
 
 supabase = SupabaseConnection()
-data, count = supabase.table("bots_dev").select("*").eq("id", "melNLS").execute()
+data, count = supabase.table("bots_dev").select("*").eq("id", "mel").execute()
 bot_info = data[1][0]
 
 def main():
